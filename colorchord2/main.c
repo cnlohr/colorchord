6dd569540385ba9ea814a5d534404a41987aed84
Eugene8388608 <zhenya.pavlenko.2004@mail.ru>
2021-11-24 00:29:42 +0300

    Add files via upload

--- colorchord2/main.c
+++ colorchord2/main.c
@@ -516,18 +516,15 @@ int main( int argc, char **argv )
 
 				CNFGColor( LINE_COLOR );
 				// Let's draw the o-scope.
-				int thissoundhead = soundhead;
-				thissoundhead = ( thissoundhead - 1 + SOUNDCBSIZE ) % SOUNDCBSIZE;
-				int lasty = sound[ thissoundhead ] * 128 + 128;
-				thissoundhead = ( thissoundhead - 1 + SOUNDCBSIZE ) % SOUNDCBSIZE;
-				int thisy = sound[ thissoundhead ] * 128 + 128;
-				thissoundhead = ( thissoundhead - 1 + SOUNDCBSIZE ) % SOUNDCBSIZE;
-				for ( int i = 0; i < screenx; i++ )
+				int thissoundhead = ( soundhead - 1 + SOUNDCBSIZE ) % SOUNDCBSIZE;
+				int lasty;
+				int thisy = sound[ thissoundhead ] * -128 + 128;
+				for ( int i = screenx - 1; i > 0; i-- )
 				{
-					CNFGTackSegment( i, lasty, i + 1, thisy );
 					lasty = thisy;
-					thisy = sound[ thissoundhead ] * 128 + 128;
+					thisy = sound[ thissoundhead ] * -128 + 128;
 					thissoundhead = ( thissoundhead - 1 + SOUNDCBSIZE ) % SOUNDCBSIZE;
+					CNFGTackSegment( i, lasty, i - 1, thisy );
 				}
 			}
 
